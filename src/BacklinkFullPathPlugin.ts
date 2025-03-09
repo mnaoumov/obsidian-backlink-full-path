74ad027bd5f979a5b8e6729846d235a033d817c2
Michael Naumov <mnaoumov@gmail.com>
2025-03-09 10:34:15 -0600

    Patch backlinks dom

--- src/BacklinkFullPathPlugin.ts
+++ src/BacklinkFullPathPlugin.ts
@@ -1,8 +1,28 @@
-import { PluginBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginBase';
-import { EmptySettings } from 'obsidian-dev-utils/obsidian/Plugin/EmptySettings';
-import type { PluginSettingTab } from 'obsidian';
+import type {
+  PluginSettingTab,
+  TFile
+} from 'obsidian';
+import type {
+  BacklinkPlugin,
+  BacklinkView,
+  ResultDom,
+  ResultDomResult,
+  TreeDom
+} from 'obsidian-typings';
 
-export class BacklinkFullPathPlugin extends PluginBase<EmptySettings> {
+import { around } from 'monkey-around';
+import { invokeAsyncSafely } from 'obsidian-dev-utils/Async';
+import { getPrototypeOf } from 'obsidian-dev-utils/Object';
+import { EmptySettings } from 'obsidian-dev-utils/obsidian/Plugin/EmptySettings';
+import { PluginBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginBase';
+import {
+  InternalPluginName,
+  ViewType
+} from 'obsidian-typings/implementations';
+
+type AddResultFn = (file: TFile, result: ResultDomResult, content: string, shouldShowTitle?: boolean) => ResultDom;
+
+export class BacklinkFullPathPlugin extends PluginBase {
   protected override createPluginSettings(): EmptySettings {
     return new EmptySettings();
   }
@@ -11,7 +31,67 @@ export class BacklinkFullPathPlugin extends PluginBase<EmptySettings> {
     return null;
   }
 
-  protected override async onLayoutReady(): Promise<void> {
+  protected override onLayoutReady(): void {
+    const backlinksCorePlugin = this.app.internalPlugins.getPluginById(InternalPluginName.Backlink);
+    if (!backlinksCorePlugin) {
+      return;
+    }
 
+    // eslint-disable-next-line consistent-this,@typescript-eslint/no-this-alias
+    const plugin = this;
+    this.register(around(getPrototypeOf(backlinksCorePlugin.instance), {
+      onUserEnable: (next: () => void) =>
+        function onUserEnablePatched(this: BacklinkPlugin): void {
+          next.call(this);
+          plugin.onBacklinksCorePluginEnable();
+        }
+    }));
+
+    if (backlinksCorePlugin.enabled) {
+      plugin.onBacklinksCorePluginEnable();
+    }
+  }
+
+  private addResult(next: AddResultFn, treeDom: TreeDom, file: TFile, result: ResultDomResult, content: string, shouldShowTitle?: boolean): ResultDom {
+    const basename = file.basename;
+    const name = file.name;
+    try {
+      file.basename = file.path;
+      file.name = file.path;
+      return next.call(treeDom, file, result, content, shouldShowTitle);
+    } finally {
+      file.basename = basename;
+      file.name = name;
+    }
+  }
+
+  private async getBacklinkView(): Promise<BacklinkView | null> {
+    const backlinksLeaf = this.app.workspace.getLeavesOfType(ViewType.Backlink)[0];
+    if (!backlinksLeaf) {
+      return null;
+    }
+
+    await backlinksLeaf.loadIfDeferred();
+    return backlinksLeaf.view as BacklinkView;
+  }
+
+  private onBacklinksCorePluginEnable(): void {
+    invokeAsyncSafely(() => this.patchBacklinksPane());
+  }
+
+  private async patchBacklinksPane(): Promise<void> {
+    const backlinkView = await this.getBacklinkView();
+    if (!backlinkView) {
+      return;
+    }
+
+    // eslint-disable-next-line consistent-this,@typescript-eslint/no-this-alias
+    const plugin = this;
+    this.register(around(getPrototypeOf(backlinkView.backlink.backlinkDom), {
+      addResult: (next: AddResultFn): AddResultFn =>
+        function addResultPatched(this: TreeDom, file, result, content, shouldShowTitle?) {
+          return plugin.addResult(next, this, file, result, content, shouldShowTitle);
+        }
+    }));
   }
 }
