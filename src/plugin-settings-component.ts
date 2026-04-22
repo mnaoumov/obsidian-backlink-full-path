c0abac43527b5644cdd368f5a85e4ef3d8346338
Michael Naumov <mnaoumov@gmail.com>
2026-04-22 11:27:42 -0600

    refactor: simplify PluginSettingsComponent constructor to accept plugin directly

--- src/plugin-settings-component.ts
+++ src/plugin-settings-component.ts
@@ -4,8 +4,6 @@
  * Settings component that manages plugin settings persistence.
  */
 
-import type { PluginSettingsComponentParams } from 'obsidian-dev-utils/obsidian/plugin/components/plugin-settings-component';
-
 import { PluginSettingsComponentBase } from 'obsidian-dev-utils/obsidian/plugin/components/plugin-settings-component';
 
 import { PluginSettings } from './plugin-settings.ts';
@@ -14,15 +12,6 @@ import { PluginSettings } from './plugin-settings.ts';
  * Manages persistence and lifecycle for {@link PluginSettings}.
  */
 export class PluginSettingsComponent extends PluginSettingsComponentBase<PluginSettings> {
-  /**
-   * Creates a new settings component.
-   *
-   * @param params - Load/save data callbacks.
-   */
-  public constructor(params: PluginSettingsComponentParams) {
-    super(params);
-  }
-
   /**
    * Creates the default settings instance.
    *
