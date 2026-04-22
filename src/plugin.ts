a3a129d30b9707cbeb790a8befa7f5b08810b9b7
Michael Naumov <mnaoumov@gmail.com>
2026-04-22 11:09:41 -0600

    refactor: pass pluginSettingsComponent instead of pluginSettings getter

--- src/plugin.ts
+++ src/plugin.ts
@@ -31,8 +31,6 @@ import {
   ViewType
 } from 'obsidian-typings/implementations';
 
-import type { PluginSettings } from './plugin-settings.ts';
-
 import { PluginSettingsComponent } from './plugin-settings-component.ts';
 import { PluginSettingsTab } from './plugin-settings-tab.ts';
 
@@ -117,10 +115,6 @@ export class Plugin extends PluginBase {
     });
   }
 
-  private get pluginSettings(): PluginSettings {
-    return this.pluginSettingsComponent.settings as PluginSettings;
-  }
-
   private addResult(next: AddResultFn, resultDom: ResultDom, file: TFile, result: ResultDomResult, content: string, shouldShowTitle?: boolean): ResultDomItem {
     const resultDomItem = next.call(resultDom, file, result, content, shouldShowTitle);
     const fileNameCaptionEl = resultDomItem.el.querySelector('.tree-item-inner');
@@ -132,40 +126,40 @@ export class Plugin extends PluginBase {
   }
 
   private generateBacklinkTitle(file: TFile): HTMLDivElement {
-    const fileNamePart = this.pluginSettings.shouldIncludeExtension ? file.name : file.basename;
+    const fileNamePart = this.pluginSettingsComponent.settings.shouldIncludeExtension ? file.name : file.basename;
 
     let parentPathParts = file.path.split('/').slice(0, -1);
 
     for (let length = parentPathParts.length; length >= 1; length--) {
       const rootPath = parentPathParts.slice(0, length).join('/');
-      if (this.pluginSettings.rootPaths.includes(rootPath)) {
+      if (this.pluginSettingsComponent.settings.rootPaths.includes(rootPath)) {
         parentPathParts = parentPathParts.slice(length);
         break;
       }
     }
 
-    if (this.pluginSettings.pathDepth > 0) {
-      const partsToSkipCount = Math.max(0, parentPathParts.length - this.pluginSettings.pathDepth + 1);
+    if (this.pluginSettingsComponent.settings.pathDepth > 0) {
+      const partsToSkipCount = Math.max(0, parentPathParts.length - this.pluginSettingsComponent.settings.pathDepth + 1);
       if (partsToSkipCount > 0) {
         parentPathParts.splice(0, partsToSkipCount);
-        if (this.pluginSettings.shouldShowEllipsisForSkippedPathParts) {
+        if (this.pluginSettingsComponent.settings.shouldShowEllipsisForSkippedPathParts) {
           parentPathParts.unshift('...');
         }
       }
     }
 
-    if (this.pluginSettings.shouldReversePathParts) {
+    if (this.pluginSettingsComponent.settings.shouldReversePathParts) {
       parentPathParts.reverse();
     }
 
-    const pathSeparator = this.pluginSettings.shouldReversePathParts ? ' \u2190 ' : '/';
+    const pathSeparator = this.pluginSettingsComponent.settings.shouldReversePathParts ? ' \u2190 ' : '/';
     const parentStr = parentPathParts.join(pathSeparator);
 
     const container = createDiv({
       cls: ['backlink-full-path', 'backlink-control']
     });
-    container.dataset['shouldHighlightFileName'] = this.pluginSettings.shouldHighlightFileName.toString();
-    container.dataset['shouldDisplayParentPathOnSeparateLine'] = this.pluginSettings.shouldDisplayParentPathOnSeparateLine.toString();
+    container.dataset['shouldHighlightFileName'] = this.pluginSettingsComponent.settings.shouldHighlightFileName.toString();
+    container.dataset['shouldDisplayParentPathOnSeparateLine'] = this.pluginSettingsComponent.settings.shouldDisplayParentPathOnSeparateLine.toString();
     container.createSpan({
       cls: 'full-path',
       text: file.path
@@ -181,14 +175,14 @@ export class Plugin extends PluginBase {
 
     if (parentStr) {
       let text = parentStr;
-      if (!this.pluginSettings.shouldDisplayParentPathOnSeparateLine) {
-        text = this.pluginSettings.shouldReversePathParts ? pathSeparator + text : text + pathSeparator;
+      if (!this.pluginSettingsComponent.settings.shouldDisplayParentPathOnSeparateLine) {
+        text = this.pluginSettingsComponent.settings.shouldReversePathParts ? pathSeparator + text : text + pathSeparator;
       }
       shadowRoot.createSpan({
         attr: {
           part: 'parent-path'
         },
-        prepend: !this.pluginSettings.shouldReversePathParts && !this.pluginSettings.shouldDisplayParentPathOnSeparateLine,
+        prepend: !this.pluginSettingsComponent.settings.shouldReversePathParts && !this.pluginSettingsComponent.settings.shouldDisplayParentPathOnSeparateLine,
         text
       });
     }
