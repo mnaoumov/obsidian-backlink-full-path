72ec7e8309fc654e9e220042c6af6bdb914f6ca7
Michael Naumov <mnaoumov@gmail.com>
2026-06-08 21:11:13 -0600

    chore: update libs

--- scripts/test-coverage.ts
+++ scripts/test-coverage.ts
@@ -1,4 +1,11 @@
 import { wrapCliTask } from 'obsidian-dev-utils/script-utils/cli-utils';
 import { testCoverage } from 'obsidian-dev-utils/script-utils/test-runners/vitest';
 
-await wrapCliTask(() => testCoverage());
+const FULL_COVERAGE_PERCENT = 100;
+
+await wrapCliTask(() =>
+  testCoverage({
+    minCoverageInPercents: FULL_COVERAGE_PERCENT,
+    projects: ['unit-tests']
+  })
+);
